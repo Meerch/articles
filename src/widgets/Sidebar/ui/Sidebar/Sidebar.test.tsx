d8ec1d4baccdd3bf78653b008c8593954e3a81c1
Meerch <khukhriansk333@gmail.com>
2022-10-14 23:41:46 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
 import { Sidebar } from './Sidebar'
-import renderWithTranslation from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import componentRender from 'shared/lib/tests/componentRender/componentRender'
 
 describe('Sidebar', () => {
     test('Test render sidebar', () => {
-        renderWithTranslation(<Sidebar/>)
+        componentRender(<Sidebar/>)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
 
     test('Test toggle sidebar', () => {
-        renderWithTranslation(<Sidebar/>)
+        componentRender(<Sidebar/>)
         const toggle = screen.getByTestId('sidebar-toggle')
         fireEvent.click(toggle)
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
